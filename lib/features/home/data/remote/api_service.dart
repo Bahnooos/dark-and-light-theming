7b350a63ae955bfc9a82066f4dfb6d9a731ce81b
Bahnas <bahnas114@gmail.com>
2025-11-01 22:46:36 +0200

    3commit

---
+++ lib/features/home/data/remote/api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dark_and_light_theming/features/home/data/models/moves_response.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../../../core/networking/api_constants.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @GET(ApiConstants.popular)
+  Future<MovesResponse> getPopularMovies(
+    @Query('page') int page, {
+    @Query('language') String language = 'en',
+  });
+}
